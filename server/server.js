03ac1fa7d14be373afeb823ab70274b0cb0d80bc
lost_fanatic <bighna2005@gmail.com>
2026-05-26 14:20:15 +0530

    Setup the express server for `phishy`

--- server/server.js
+++ server/server.js
@@ -1,1 +1,24 @@
-/// server.js
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
+
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/admin.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
